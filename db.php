23a378ffb4b1113c1451e42a78bc5c4c484f782c
HonzaBalvan <romanovsky.honza@gmail.com>
2023-06-02 13:23:29 +0200

    meow meow mutherfucker

--- db.php
+++ db.php
@@ -1,1 +1,33 @@
-<?php  phpinfo();?>
+<?php
+
+$server_name = "localhost";
+
+$user_name = "root";
+
+$password = "";
+
+$database_name = "konawiec_sql";
+
+$connection = mysqli_connect($server_name, $user_name, $password, $database_name);
+
+if (!$connection) {
+  die("Failed ". mysqli_connect_error());
+}
+echo "Connection established successfully <br>" ;
+
+$query = "select * from produkty";
+
+$final = mysqli_query($connection, $query);
+
+if (mysqli_num_rows($final) > 0) {
+ //get the output of each row
+  while($i = mysqli_fetch_assoc($final)) {
+      //get id and name columns
+    // vypissuje, nepotřebuju --
+    echo "id: " . $i["id"] . ", jmeno: " . $i["jmeno"] . ", popis: " . $i["popis"] . ", cena: " . $i["cena"] . ", skladem: " . $i["skladem"] . "<br>";
+  }
+} else {
+  echo "No results";
+}
+
+?>
